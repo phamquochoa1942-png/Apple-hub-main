9d304b034b080f2eeec1bc0b778eaf659c984f1a
phamquochoa1942-png <phamquochoa1942@gmail.com>
2026-04-02 15:43:20 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5,7 +5,7 @@ local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/phamquocho
 local window = UI:CreateWindow({
     Title = "Apple Hub Premium",
     Subtitle = "| by Quoc Hoa",
-    Image = "rbxassetid://88742994396030"
+    Image = "rbxassetid://82042449818006"
 })
 
 -- ==================== TẠO TAB ====================
